d775b67c80db6b42ea0b614a18d27b901acb11da
curso09 <curso09@nomail.com>
2024-09-12 17:46:37 +0200

    hashCode, equals y toString de los entity Ingrediente y Plato

--- src/main/java/es/cic/curso09/nomasdietas/model/Ingrediente.java
+++ src/main/java/es/cic/curso09/nomasdietas/model/Ingrediente.java
@@ -25,7 +25,8 @@ public class Ingrediente {
     @NotNull
     @Size(min = 1, max = 64)
     private String nombre;
-    @Size(min = 1, max = 256)
+
+    @Size(max = 256)
     private String descripcion;
 
     // Unidades por cada 100 gramos de ingrediente
@@ -100,6 +101,68 @@ public class Ingrediente {
 
     public void setPlatos(List<Plato> platos) {
         this.platos = platos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+        result = prime * result + Float.floatToIntBits(calorias);
+        result = prime * result + Float.floatToIntBits(carbohidratos);
+        result = prime * result + Float.floatToIntBits(proteinas);
+        result = prime * result + Float.floatToIntBits(grasas);
+        result = prime * result + ((platos == null) ? 0 : platos.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Ingrediente other = (Ingrediente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (descripcion == null) {
+            if (other.descripcion != null)
+                return false;
+        } else if (!descripcion.equals(other.descripcion))
+            return false;
+        if (Float.floatToIntBits(calorias) != Float.floatToIntBits(other.calorias))
+            return false;
+        if (Float.floatToIntBits(carbohidratos) != Float.floatToIntBits(other.carbohidratos))
+            return false;
+        if (Float.floatToIntBits(proteinas) != Float.floatToIntBits(other.proteinas))
+            return false;
+        if (Float.floatToIntBits(grasas) != Float.floatToIntBits(other.grasas))
+            return false;
+        if (platos == null) {
+            if (other.platos != null)
+                return false;
+        } else if (!platos.equals(other.platos))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingrediente [nombre=" + nombre + ", descripcion=" + descripcion + ", calorias=" + calorias
+                + ", carbohidratos=" + carbohidratos + ", proteinas=" + proteinas + ", grasas=" + grasas + ", platos="
+                + platos + "]";
     }
 
 }
--- src/main/java/es/cic/curso09/nomasdietas/model/Plato.java
+++ src/main/java/es/cic/curso09/nomasdietas/model/Plato.java
@@ -1,10 +1,5 @@
 package es.cic.curso09.nomasdietas.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -27,7 +22,7 @@ public class Plato {
     @Size(min = 1, max = 64)
     private String nombre;
 
-    @Size(min = 1, max = 256)
+    @Size(max = 256)
     private String descripcion;
 
     @Size(max = 64)
@@ -46,6 +41,7 @@ public class Plato {
     @Min(0)
     private float grasas;
 
+    @NotNull
     private boolean favorito = false;
 
     // @NotNull
@@ -136,6 +132,82 @@ public class Plato {
 
     public void setIngrediente(Ingrediente ingrediente) {
         this.ingrediente = ingrediente;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+        result = prime * result + ((otrosIngredientes == null) ? 0 : otrosIngredientes.hashCode());
+        result = prime * result + Float.floatToIntBits(cantidadIngredientePrincipal);
+        result = prime * result + Float.floatToIntBits(calorias);
+        result = prime * result + Float.floatToIntBits(carbohidratos);
+        result = prime * result + Float.floatToIntBits(proteinas);
+        result = prime * result + Float.floatToIntBits(grasas);
+        result = prime * result + (favorito ? 1231 : 1237);
+        result = prime * result + ((ingrediente == null) ? 0 : ingrediente.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Plato other = (Plato) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (descripcion == null) {
+            if (other.descripcion != null)
+                return false;
+        } else if (!descripcion.equals(other.descripcion))
+            return false;
+        if (otrosIngredientes == null) {
+            if (other.otrosIngredientes != null)
+                return false;
+        } else if (!otrosIngredientes.equals(other.otrosIngredientes))
+            return false;
+        if (Float.floatToIntBits(cantidadIngredientePrincipal) != Float
+                .floatToIntBits(other.cantidadIngredientePrincipal))
+            return false;
+        if (Float.floatToIntBits(calorias) != Float.floatToIntBits(other.calorias))
+            return false;
+        if (Float.floatToIntBits(carbohidratos) != Float.floatToIntBits(other.carbohidratos))
+            return false;
+        if (Float.floatToIntBits(proteinas) != Float.floatToIntBits(other.proteinas))
+            return false;
+        if (Float.floatToIntBits(grasas) != Float.floatToIntBits(other.grasas))
+            return false;
+        if (favorito != other.favorito)
+            return false;
+        if (ingrediente == null) {
+            if (other.ingrediente != null)
+                return false;
+        } else if (!ingrediente.equals(other.ingrediente))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Plato [nombre=" + nombre + ", descripcion=" + descripcion + ", otrosIngredientes=" + otrosIngredientes
+                + ", cantidadIngredientePrincipal=" + cantidadIngredientePrincipal + ", calorias=" + calorias
+                + ", carbohidratos=" + carbohidratos + ", proteinas=" + proteinas + ", grasas=" + grasas + ", favorito="
+                + favorito + ", ingrediente=" + ingrediente + "]";
     }
 
 }
